320467a9b69280b9efb317c1d77305b2eeb7d064
Shaham123 <89772416+Shaham123@users.noreply.github.com>
2021-10-06 05:29:19 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = "@TeamAllmoviessearchitake",
+                caption = "@malllumoviesgroups",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
